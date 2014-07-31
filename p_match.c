eda058c2453eb06941e488044d6028e00b3048e7
Dominic Moylett <dm1905@bristol.ac.uk>
2014-07-31 15:56:52 +0100

    Implemented parametrised matching.

--- p_match.c
+++ p_match.c
@@ -11,11 +11,30 @@ int compare_string(char* A, char* B, int m) {
     return 1;
 }
 
+void test_match(char* T, int n, char* P, int m, char* sigma, int s_sigma, char* pi, int s_pi, int correct_matches, int* correct) {
+    int* output = malloc((n - m + 1) * sizeof(int));
+    int matches = p_match(T, n, P, m, sigma, s_sigma, pi, s_pi, output);
+    assert(correct_matches == matches && compare(output, correct, matches));
+    if (matches > 0) free(output);
+}
+
 int main(void) {
     int n = 10, m = 3;
     char* preprocessed = malloc(n * sizeof(char));
     preprocess("abcabcdeab", n, "abc", m, preprocessed);
     assert(compare_string(preprocessed, "abcabcddab", 10));
+
+    n = 18; m = 5;
+    int correct_matches = 1;
+    int* correct = malloc(correct_matches * sizeof(int));
+    correct[0] = 8;
+    test_match("ababaabbababcababd", n, "ababc", m, "cd", 2, "ab", 2, correct_matches, correct);
+
+    n = 18; m = 5;
+    correct_matches = 0;
+    correct = realloc(correct, correct_matches * sizeof(int));
+    test_match("ababaabbababdababd", n, "ababc", m, "cd", 2, "ab", 2, correct_matches, correct);
+
     printf("All tests succeeded!\n");
     return 0;
 }
--- p_match.h
+++ p_match.h
@@ -1,5 +1,9 @@
 #include "trees/red_black_tree/red_black_tree/rbtree.c"
 #include "shared_funcs.h"
+#include "kmp.h"
+#include "m_match.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void preprocess(char* T, int n, char* sigma, int s_sigma, char* T_p) {
     char a = 0;
@@ -13,4 +17,40 @@ void preprocess(char* T, int n, char* sigma, int s_sigma, char* T_p) {
     for (i = 0; i < n; i++) {
         T_p[i] = (rbtree_lookup(symbols, (void*)T[i], (void*)0, compare_char) == 0) ? a : T[i];
     }
+}
+
+int p_match(char* T, int n, char* P, int m, char* sigma, int s_sigma, char* pi, int s_pi, int* output) {
+    char* T_p = malloc(n * sizeof(char));
+    char* T_pp = malloc(n * sizeof(char));
+    char* P_p = malloc(m * sizeof(char));
+    char* P_pp = malloc(m * sizeof(char));
+    preprocess(T, n, sigma, s_sigma, T_p);
+    preprocess(P, m, sigma, s_sigma, P_p);
+    preprocess(T, n, pi, s_pi, T_pp);
+    preprocess(P, m, pi, s_pi, P_pp);
+
+    int* static_match = malloc((n - m + 1) * sizeof(int));
+    int* variable_match = malloc((n - m + 1) * sizeof(int));
+
+    int static_cnt = kmp_match(T_p, n, P_p, m, static_match);
+    int variable_cnt = mmatch_match(T_pp, n, P_pp, m, variable_match);
+
+    int i = 0, j = 0;
+    int matches = 0;
+    while ((i < static_cnt) && (j < variable_cnt)) {
+        if (static_match[i] == variable_match[j]) {
+            output[matches++] = static_match[i];
+            i++;
+            j++;
+        } else if (static_match[i] > variable_match[j]) i++;
+        else j++;
+    }
+    output = realloc(output, matches * sizeof(int));
+    free(T_p);
+    free(T_pp);
+    free(P_p);
+    free(P_pp);
+    free(static_match);
+    free(variable_match);
+    return matches;
 }
