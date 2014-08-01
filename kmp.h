ec7153dd24c4646af049ee9d3be05d81f29904b3
Dominic Moylett <dm1905@bristol.ac.uk>
2014-08-01 11:16:16 +0100

    Removed unneeded conversion.

--- kmp.h
+++ kmp.h
@@ -64,7 +64,7 @@ int kmp_match(char* T, int n, char* P, int m, int* output) {
             i = failure[i - 1];
         }
     }
-    output = (int*) realloc(output, matches * sizeof(int));
+    output = realloc(output, matches * sizeof(int));
     free(failure);
     return matches;
 }
